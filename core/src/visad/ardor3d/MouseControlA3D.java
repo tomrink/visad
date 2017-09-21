8cf5640435d02330901f04c3d749493712d5c251
rink <rink@moonunit.ssec.wisc.edu>
2017-09-21 10:06:10 -0500

    intial commit

---
+++ core/src/visad/ardor3d/MouseControlA3D.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visad.ardor3d;
+
+import com.ardor3d.scenegraph.Node;
+
+/**
+ *
+ * @author rink
+ */
+class MouseControlA3D {
+
+   public MouseControlA3D(Node transform) {
+   }
+   
+}
